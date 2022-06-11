1ecbeb24665eab1a4957b136bcd59feffaf81e08
joshuaweiss2020 <3373182@qq.com>
2022-06-11 12:37:13 +0800

    Initial commit

---
+++ src/main/java/com/joshua/spring2/School.java
@@ -0,0 +1,36 @@
+package com.joshua.spring2;
+
+public class School
+{
+    private  String name;
+    private String address;
+
+    public School() {
+        System.out.println("School 无参构照 ");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
+
---
+++ src/test/java/com/joshua/spring/test/MyTest.java
@@ -0,0 +1,21 @@
+package com.joshua.spring.test;
+
+import com.joshua.spring.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public  void testStudent(){
+        Student stu = new Student();
+        System.out.println(stu);
+    }
+    @Test
+    public void testStudentSpring(){
+        ApplicationContext ac =new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student stu = (Student) ac.getBean("stu");
+        System.out.println(stu);
+
+    }
+}
